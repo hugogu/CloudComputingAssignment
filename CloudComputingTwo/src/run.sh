7789864a452011fde1badb20fa2edff767aa5c65
Hugo Gu <hugogu@outlook.com>
2015-09-04 14:19:57 +0000

    Add a run script

---
+++ CloudComputingTwo/src/run.sh
@@ -0,0 +1,8 @@
+echo "Building and Running $1.java"
+rm -rvf ./build/* ./$1.jar
+mkdir -vp build
+export HADOOP_CLASS=$JAVA_HOME/lib/tools.jar
+hadoop com.sun.tools.javac.Main $1.java -d build
+jar -cvf $1.jar -C build/ ./
+hadoop fs -rm -r -f /mp2/$2-output
+hadoop jar $1.jar $1 -D stopwords=/mp2/misc/stopwords.txt -D delimiters=/mp2/misc/delimiters.txt /mp2/titles /mp2/$2-output
