f63f4bba884015abf296d8cf9392187c2b952337
Hugo Gu <hugogu@outlook.com>
2015-09-05 12:28:49 +0800

    Fix HADOOP_CLASSPATH in build script.

--- CloudComputingTwo/src/build.sh
+++ CloudComputingTwo/src/build.sh
@@ -1,7 +1,7 @@
 echo "Building $1.java"
 rm -rvf ./build/* ./$1.jar
 mkdir -vp build
-export HADOOP_CLASS=$JAVA_HOME/lib/tools.jar
+export HADOOP_CLASSPATH=$JAVA_HOME/lib/tools.jar
 hadoop com.sun.tools.javac.Main $1.java -d build
 jar -cvf $1.jar -C build/ ./
 hadoop fs -rm -r -f /mp2/$2-output
