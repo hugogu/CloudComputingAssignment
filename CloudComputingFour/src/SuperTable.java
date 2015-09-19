62511e4ee704832d39398cf4110c9b3fff57093f
Hugo Gu <hugogu@outlook.com>
2015-09-19 21:06:17 +0800

    Try answer assignment four.

--- CloudComputingFour/src/SuperTable.java
+++ CloudComputingFour/src/SuperTable.java
@@ -17,46 +17,37 @@ import org.apache.hadoop.hbase.client.Scan;
 
 import org.apache.hadoop.hbase.util.Bytes;
 
-public class SuperTable{
+public class SuperTable {
 
-   public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
+        final Configuration con = HBaseConfiguration.create();
+        final HBaseAdmin admin = new HBaseAdmin(con);
+        final HTableDescriptor tableDescriptor = new HTableDescriptor(TableName.valueOf("powers"));
+        tableDescriptor.addFamily(new HColumnDescriptor("personal"));
+        tableDescriptor.addFamily(new HColumnDescriptor("professional"));
+        admin.createTable(tableDescriptor);
 
-      // Instantiate Configuration class
+        final HTable table = new HTable(con, "powers");
+        addRow(table, "row1", "superman", "strength", "clark", "100");
+        addRow(table, "row2", "batman", "money", "bruce", "50");
+        addRow(table, "row3", "wolverine", "healing", "logan", "75");
+        table.close();
 
-      // Instaniate HBaseAdmin class
-      
-      // Instantiate table descriptor class
+        final Scan scan = new Scan();
+        scan.addColumn(Bytes.toBytes("personal"), Bytes.toBytes("hero"));
+        final ResultScanner scanner = table.getScanner(scan);
+        for (Result result : scanner) {
+            System.out.println(result);
+        }
+        scanner.close();
+    }
 
-      // Add column families to table descriptor
-
-      // Execute the table through admin
-
-      // Instantiating HTable class
-     
-      // Repeat these steps as many times as necessary
-
-	      // Instantiating Put class
-              // Hint: Accepts a row name
-
-      	      // Add values using add() method
-              // Hints: Accepts column family name, qualifier/row name ,value
-
-      // Save the table
-	
-      // Close table
-
-      // Instantiate the Scan class
-     
-      // Scan the required columns
-
-      // Get the scan result
-
-      // Read values from scan result
-      // Print scan result
- 
-      // Close the scanner
-   
-      // Htable closer
-   }
+    private static void addRow(HTable table, String row, String hero, String power, String name, String xp) throws IOException {
+        final Put put = new Put(Bytes.toBytes(row));
+        put.addColumn(Bytes.toBytes("personal"), Bytes.toBytes("hero"), Bytes.toBytes(hero));
+        put.addColumn(Bytes.toBytes("personal"), Bytes.toBytes("power"), Bytes.toBytes(power));
+        put.addColumn(Bytes.toBytes("professional"), Bytes.toBytes("name"), Bytes.toBytes(name));
+        put.addColumn(Bytes.toBytes("professional"), Bytes.toBytes("xp"), Bytes.toBytes(xp));
+        table.put(put);
+    }
 }
-
